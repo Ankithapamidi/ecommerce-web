53571c909bd28651c200e77cda36cc3fe3484e38
Ankithapamidi <paamidiankitha@gmail.com>
2025-09-05 21:33:15 +0530

    First commit

---
+++ src/main/java/com/Ankitha/ecom_proj/repo/ProductRepo.java
@@ -0,0 +1,10 @@
+package com.Ankitha.ecom_proj.repo;
+
+import com.Ankitha.ecom_proj.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product,Integer> {
+
+}
